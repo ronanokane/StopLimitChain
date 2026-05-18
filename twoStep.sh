f07cd10594358186442f5db70ba584fafc47f693
ronanokane <ronan.okane@gmail.com>
2026-05-18 04:35:19 +0100

    Initial commit

---
+++ twoStep.sh
@@ -0,0 +1,34 @@
+operation=$1
+firstStepSymbol=$2
+secondStepSymbol=$3
+percentage=$4
+
+if [ "$operation" = "BUY" ]; then
+    action='./buyAsset.sh'
+elif [ "$operation" = "SELL" ]; then
+    action='./sellAsset.sh'
+fi
+
+if [ "$#" -ne 4 ] || [ -z "$action" ] || [[ "$firstStepSymbol" != *"/"* ]] || [[ "$secondStepSymbol" != *"/"* ]]; then
+    echo "Usage: ./${0##*/} <OPERATION> <STEP1_SYMBOL> <STEP2_SYMBOL> <PERCENTAGE_TO_BUY_OR_SELL>" >&2
+    echo >&2
+    echo "Examples:" >&2
+    echo "  ./${0##*/} BUY ETH/USDC ETH/ZEC 100" >&2
+    echo "  ./${0##*/} SELL ETH/USDC ETH/ZEC 100" >&2
+    echo >&2
+    exit 1
+fi
+
+amountField=$([[ "$action" == "./buyAsset.sh" ]] && echo ".executedQty" || echo ".cummulativeQuoteQty")
+json=$("$action" "$firstStepSymbol" "$percentage" 2>/dev/null)
+
+if [ $? -eq 0 ] && [ -n "$json" ]; then
+    local amount=$(echo "$json" | jq -r "$amountField")
+
+    if [ $? -eq 0 ] && "$action" "$secondStepSymbol" -a "$amount"; then
+        exit 0
+    fi
+fi
+
+echo "Error executing one or more of orders..." 2>/dev/null
+exit 1
